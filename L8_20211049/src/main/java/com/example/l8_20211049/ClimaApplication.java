b55e66cd7209a95f4c40ef6d59f1e7f7bc86276f
AlexiaPT <apalaciost@pucp.edu.pe>
2025-06-13 21:32:01 -0500

    Update ClimaApplication.java

--- L8_20211049/src/main/java/com/example/l8_20211049/ClimaApplication.java
+++ L8_20211049/src/main/java/com/example/l8_20211049/ClimaApplication.java
@@ -1,4 +1,11 @@
 package com.example.l8_20211049;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ClimaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClimaApplication.class, args);
+    }
 }
